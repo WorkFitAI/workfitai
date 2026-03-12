743ad0543aa44d1b760eece43636ad5a5dd74e63
lightningflashh <thanhchi0429@gmail.com>
2026-03-12 10:23:12 +0700

    feat: Update image configuration to use remotePatterns for better security

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["res.cloudinary.com", "example.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+      },
+    ],
   },
 };
 
